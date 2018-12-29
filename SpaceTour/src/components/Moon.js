eb33d1633834b07a9423012ad3d915f7214180b8
kosuke shinmura <justasiam69koke@gmail.com>
2018-12-29 13:22:24 +0900

    training

---
+++ SpaceTour/src/components/Moon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Sphere, asset } from 'react-vr';
+
+const Moon = props => (
+<Sphere
+lit={true}
+widthSegments={20}
+heightSegments={12}
+texture={asset('2k_moon.jpg')}
+{...props}
+/>
+);
+
+export default Moon;
